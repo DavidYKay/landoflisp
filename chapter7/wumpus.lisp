b0f62d3d9a9b0f2c3995caaf9fc007129123261c
David Young-Chan Kay <davidykay@gmail.com>
2011-07-26 23:57:43 -0400

    User known city map generated.

--- chapter7/wumpus.lisp
+++ chapter7/wumpus.lisp
@@ -128,7 +128,8 @@
   (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
   (setf *player-pos* (find-empty-node))
   (setf *visited-nodes* (list *player-pos*))
-  (draw-city))
+  (draw-city)
+  (draw-known-city))
 
 (defun find-empty-node ()
   (let ((x (random-node)))
@@ -138,3 +139,33 @@
 
 (defun draw-city ()
   (ugraph->png "city" *congestion-city-nodes* *congestion-city-edges*))
+
+; User Map Management
+(defun known-city-nodes ()
+  (mapcar (lambda (node)
+            (if (member node *visited-nodes*)
+              (let ((n (assoc node *congestion-city-nodes*)))
+                (if (eql node *player-pos*)
+                  (append n '(*))
+                  n))
+              (list node '?)))
+          (remove-duplicates
+            (append *visited-nodes*
+                    (mapcan (lambda (node)
+                              (mapcar #'car
+                                      (cdr (assoc node
+                                                  *congestion-city-edges*))))
+                            *visited-nodes*)))))
+
+(defun known-city-edges ()
+  (mapcar (lambda (node)
+            (cons node (mapcar (lambda (x)
+                                 (if (member (car x) *visited-nodes*)
+                                   x
+                                   (list (car x))))
+                               (cdr (assoc node *congestion-city-edges*)))))
+          *visited-nodes*))
+
+(defun draw-known-city ()
+  (ugraph->png "known-city" (known-city-nodes) (known-city-edges)))
+
